3471100336ead670cd1937f458bbb76dd7424e7b
jcgn <juancarlosdev91@gmail.com>
2021-04-05 12:57:27 -0600

    adding update changed

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,11 +6,13 @@ const ImageLinkForm = () => {
       <p className="f3">
         {"This Magic Brain will detect faces in your pictures. Git it a try."}
       </p>
-      <div className="f3">
-        <input className="f4 pa2 w-70 center" type="text" />
-        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
-          Detect
-        </button>
+      <div className="center">
+        <div className="center pa4 br3 shadow-5">
+          <input className="f4 pa2 w-70 center" type="text" />
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+            Detect
+          </button>
+        </div>
       </div>
     </div>
   );
