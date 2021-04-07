20c307ee5238e050a1ec6b18e8c53deda2b15805
jcgn <juancarlosdev91@gmail.com>
2021-04-07 16:56:20 -0600

    adding update change

--- src/App.js
+++ src/App.js
@@ -19,6 +19,21 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onSubmit = () => {
+    console.log("click");
+  };
+
   render() {
     return (
       <div className="App">
@@ -26,7 +41,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit />
         {/*   <FaceRecognition />*/}
       </div>
     );
