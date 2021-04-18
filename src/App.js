fd29c733dd1c64fbc832c321cbf1ad6a2ecc4372
jcgn <juancarlosdev91@gmail.com>
2021-04-18 10:21:53 -0600

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: "7912ed6e1d974b1eb6b08aeee5c01c3c",
+});
 
 const particlesOptions = {
   particles: {
@@ -32,6 +37,19 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log("click");
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {
+          // there was an error
+        }
+      );
   };
 
   render() {
