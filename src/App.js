47d7a2edacc4b846ef212ca49ec7724841586b2b
jcgn <juancarlosdev91@gmail.com>
2021-04-18 11:09:18 -0600

    adding change update

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
     console.log("click");
     app.models
       .predict(
-        "a403429f2ddf4b49b307e318f00e528b",
+        Clarifai.COLOR_MODEL,
         "https://samples.clarifai.com/face-det.jpg"
       )
       .then(
