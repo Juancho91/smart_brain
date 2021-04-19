517147ecd546b6861b2b270c6bfde642a441929f
jcgn <juancarlosdev91@gmail.com>
2021-04-19 13:22:04 -0600

    adding updates for css

--- src/App.js
+++ src/App.js
@@ -30,8 +30,11 @@ class App extends Component {
     this.state = {
       input: "",
       imageUrl: "",
+      box: {},
     };
   }
+
+  calculateFaceLocation = (data) => {};
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
@@ -39,16 +42,10 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function (err) {
-        // there was an error
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => this.calculateFaceLocation(response))
+      .catch((err) => console.log(err));
   };
 
   render() {
